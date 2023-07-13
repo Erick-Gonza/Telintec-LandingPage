096af13aa798c9f428e6fd05408934323ca64d78
Erick Gzz <eagm080897@gmail.com>
2023-07-13 09:22:52 -0600

    Add svg for theme switcher

--- src/app/components/ThemeSwitcher.tsx
+++ src/app/components/ThemeSwitcher.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
+import { sun, moon } from "@/app/assets";
+import Image from "next/image";
 
 const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
@@ -16,9 +18,21 @@ const ThemeSwitcher = () => {
   }
 
   return (
-    <div className="absolute bottom-0 right-4">
+    <div className="fixed bottom-4 right-4 sm:bottom-[100px] sm:right-8 z-10">
       <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        Toggle Theme
+        {theme === "dark" ? (
+          <Image
+            src={sun}
+            alt="logo"
+            className="w-[32px] h-[32px] object-contain"
+          />
+        ) : (
+          <Image
+            src={moon}
+            alt="logo"
+            className="w-[32px] h-[32px] object-contain"
+          />
+        )}
       </button>
     </div>
   );
