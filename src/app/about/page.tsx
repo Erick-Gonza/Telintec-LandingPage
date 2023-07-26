d8efd9b164dc96743f4e0f4f9787cdcb857295a0
Erick Gzz <eagm080897@gmail.com>
2023-07-26 17:46:44 -0600

    Fix about responsive and styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,71 +9,72 @@ export default function page() {
   return (
     <Wrapper>
       <main className={`${styles.container}`}>
-        <section
-          id="home"
-          className={`flex md:flex-row flex-col ${styles.paddingY}`}
-        >
+        <section className={`${styles.boxWidth}`}>
           <section
-            className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+            id="home"
+            className={`flex md:flex-row flex-col ${styles.paddingY}`}
           >
-            <div className="flex flex-row justify-between items-center w-full">
-              <h2 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[48px] ss:leading-[100px] leading-[75px] text-text dark:text-text-dark">
-                {title}
-              </h2>
-            </div>
-            <p
-              className={`${styles.paragraph} max-w-[470px] mt-5 font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[28px] leading-[21px] text-text-off dark:text-text-dark-off`}
+            <section
+              className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
             >
-              {description}
+              <div className="flex flex-row justify-between items-center w-full">
+                <h2 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[48px] ss:leading-[100px] leading-[75px] text-text dark:text-text-dark">
+                  {title}
+                </h2>
+              </div>
+              <p
+                className={`${styles.paragraph} max-w-[470px] mt-5 font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[28px] leading-[21px] text-text-off dark:text-text-dark-off`}
+              >
+                {description}
+              </p>
+            </section>
+
+            <section
+              className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+            >
+              <Image
+                src={robot}
+                alt="billing"
+                className="w-[100%] h-[100%] relative z-[5] object-contain"
+              />
+            </section>
+          </section>
+
+          <section className={`flex flex-col gap-4 py-4 px-6 xl:p-0`}>
+            <h2 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
+              {about[0]}
+            </h2>
+            <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off">
+              {about[1]}
             </p>
+
+            <h3 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
+              {mision[0]}
+            </h3>
+            <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off">
+              {mision[1]}
+            </p>
+            <h3 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
+              {vision[0]}
+            </h3>
+            <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off">
+              {vision[1]}
+            </p>
+            <h3 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
+              {pilares.title}
+            </h3>
+            <ul className="flex flex-col list-none font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off mb-6">
+              {pilares.values.map((value, index) => (
+                <li key={index} className="flex items-center gap-2 p-2">
+                  <div className="w-2 h-2 bg-primary rounded-full mr-2 animate-pulse"></div>
+                  <span>{value}</span>
+                </li>
+              ))}
+            </ul>
           </section>
 
-          <section
-            className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
-          >
-            <Image
-              src={robot}
-              alt="billing"
-              className="w-[100%] h-[100%] relative z-[5] object-contain"
-            />
-          </section>
+          <ContactCard />
         </section>
-
-        <section
-          className={`flex flex-col gap-4 ${styles.paddingY} ${styles.paddingX}`}
-        >
-          <h2 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
-            {about[0]}
-          </h2>
-          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off">
-            {about[1]}
-          </p>
-
-          <h3 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
-            {mision[0]}
-          </h3>
-          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off">
-            {mision[1]}
-          </p>
-          <h3 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
-            {vision[0]}
-          </h3>
-          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off">
-            {vision[1]}
-          </p>
-          <h3 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-text dark:text-text-dark">
-            {pilares.title}
-          </h3>
-          <ul className="flex flex-col list-none font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-text-off dark:text-text-dark-off">
-            {pilares.values.map((value, index) => (
-              <li key={index} className="flex items-center gap-2">
-                <span>{value}</span>
-              </li>
-            ))}
-          </ul>
-        </section>
-
-        <ContactCard />
       </main>
     </Wrapper>
   );
